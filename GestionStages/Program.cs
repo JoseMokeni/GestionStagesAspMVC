1df953ccd652aa7ed45b8a4e1105fd1f2d9187c0
José Mokeni <jmmokeni@gmail.com>
2024-05-07 16:05:09 +0100

    role control ok everywhere

--- GestionStages/Program.cs
+++ GestionStages/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
